545ddbf9d2c309ef96d3ebf78e7636a1bac96773
pivnati <natanmuleta911@gmail.com>
2025-07-05 14:51:22 -0700

    create the lockicon and login.png

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,28 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import LoginRegister from './pages/LoginRegister';
-import Notes from './pages/Notes';
+import { useEffect, useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { account } from "./appwrite/config";
+import Notes from "./pages/Notes";
+import LoginRegister from "./pages/LoginRegister";
+import { PrivateRoutes } from "./components/PrivateRoutes";
+import AuthProvider from "./context/AuthContext";
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<Notes />} />
-        <Route path='/login' element={<LoginRegister />} />
-      </Routes>
-    </BrowserRouter>
-  );
+    return (
+        <div id="app" data-theme="dark">
+            <div id="container">
+                <AuthProvider>
+                    <BrowserRouter>
+                        <Routes>
+                            <Route element={<LoginRegister />} path="/login" />
+                            <Route element={<PrivateRoutes />}>
+                                <Route element={<Notes />} path="/" />
+                            </Route>
+                        </Routes>
+                    </BrowserRouter>
+                </AuthProvider>
+            </div>
+        </div>
+    );
 }
 
 export default App;
--- src/appwrite/config.js
+++ src/appwrite/config.js
@@ -1,9 +1,12 @@
-import { Client, Databases } from "appwrite";
+import { Client, Databases, Account } from "appwrite";
 
 const client = new Client();
 
 client
-  .setEndpoint(import.meta.env.VITE_ENDPOINT) // Or your self-hosted URL
-  .setProject(import.meta.env.VITE_PROJECT_ID); // Replace with your actual project ID
+    .setEndpoint(import.meta.env.VITE_ENDPOINT)
+    .setProject(import.meta.env.VITE_PROJECT_ID);
+
 const databases = new Databases(client);
-export { client, databases };
+const account = new Account(client);
+
+export { client, databases, account };
